684a1584c20d6e8be5450bee2a37b0b12f6b2428
LontchiFrank <fodjof6@gmail.com>
2023-01-04 00:08:56 +0100

    created routes

--- route/poemPost_route.js
+++ route/poemPost_route.js
@@ -11,4 +11,30 @@ router.get("/", async (req, res) => {
   }
 });
 
+// create new poem
+
+router.post("/new-poem", async (req, res) => {
+  const newPoem = new Poem(req.body);
+  try {
+    const savePoem = await newPoem.save();
+    res.status(200).json(savePoem);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//update the poem
+router.put("/:id", async (req, res) => {
+  try {
+    const updatePoem = await Poem.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatePoem);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
